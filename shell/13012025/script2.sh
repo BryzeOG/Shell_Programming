933bde36bc9389578e7964843a5eb68be8d50f26
BryzeOG <119498331+BryzeOG@users.noreply.github.com>
2025-01-13 12:37:16 +0530

    Update script2.sh

--- shell/13012025/script2.sh
+++ shell/13012025/script2.sh
@@ -5,6 +5,7 @@ sum=0
 read -p "Enter the number " n
 while [ $n -gt 0 ]
 do	
+# Extracts and stores the remainder, this we get the last digit, then it is added to the sum and the number is divided by 10 to get rid of the bymber
 	temp=`expr $n % 10`
 	sum=`expr $sum + $temp`
 	n=`expr $n / 10`
